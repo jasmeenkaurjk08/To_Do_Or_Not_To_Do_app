05911fc22f15ab794a824680659e4f031cd6a700
jasmeenkaur <jasmeen.kaur@ucdenver.edu>
2023-07-24 22:51:04 -0600

    new layouts

--- app/src/main/java/edu/ucdenver/jasmeenkaur/todoornottodoapp/AddTask.java
+++ app/src/main/java/edu/ucdenver/jasmeenkaur/todoornottodoapp/AddTask.java
@@ -1,4 +1,22 @@
 package edu.ucdenver.jasmeenkaur.todoornottodoapp;
 
-public class AddTask {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import edu.ucdenver.jasmeenkaur.todoornottodoapp.databinding.AddTaskBinding;
+
+public class AddTask extends AppCompatActivity {
+    private AppBarConfiguration appBarConfiguration;
+    private AddTaskBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = AddTaskBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.toolbarAddTask.inflateMenu(R.menu.menu_back);
+    }
 }
